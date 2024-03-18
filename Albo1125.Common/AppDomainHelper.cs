84c4d3e5096a7422009746fe88184a90c5ff3429
Luca Taschner <iMortalTrix@Proton.me>
2024-03-18 13:55:12 +0100

    Remove unused using directives in AppDomainHelper.cs
    The using directives for System, System.Collections.Generic, System.Linq, System.Text, and System.Threading.Tasks were removed from AppDomainHelper.cs as they were not needed in the present code.

--- Albo1125.Common/AppDomainHelper.cs
+++ Albo1125.Common/AppDomainHelper.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Albo1125.Common
+﻿namespace Albo1125.Common
 {
     //Thank you LMS for your help in writing this class.
     using System;
